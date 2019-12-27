4f9ca4f1cd9e2db6ecd0dfa0ea2a516821db8d87
onofrej <onofre.junior@gmail.com>
2019-12-27 23:26:45 +0000

    test: added unit tests.

---
+++ Tests/DirectionsReductor.Application.Services.Tests/Reducer/ReducerServiceTests.cs
@@ -0,0 +1,54 @@
+﻿namespace DirectionsReductor.Application.Services.Tests.Reducer
+{
+    using AutoFixture;
+    using DirectionsReductor.Application.Services.Reducer;
+    using DirectionsReductor.Application.Services.Validation;
+    using FluentAssertions;
+    using NSubstitute;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    public class ReducerServiceTests
+    {
+        private readonly Fixture fixture;
+        private readonly IReducerService reducerService;
+
+        private readonly IValidationService validationService;
+
+        public ReducerServiceTests()
+        {
+            fixture = new Fixture();
+            validationService = Substitute.For<IValidationService>();
+            reducerService = new ReducerService(validationService);
+        }
+
+        [Fact]
+        public void Reduce_InvalidInputDirections_ThrowsException()
+        {
+            // Arrange
+            var directions = fixture.CreateMany<string>();
+
+            // Act
+            var result = reducerService.Reduce(directions.ToList());
+
+            // Assert
+            Assert.Throws<System.Collections.Generic.KeyNotFoundException>(() => reducerService.Reduce(directions.ToList()));
+        }
+
+        [Fact]
+        public void Reduce_ValidInputDirections_DirectionReduced()
+        {
+            // Arrange
+            var directions = new List<string> { "west", "north", "south" };
+
+            // Act
+            var result = reducerService.Reduce(directions);
+
+            // Assert
+            result.First().Should().NotBeNullOrEmpty();
+            result.First().Should().Be(directions.First());
+            validationService.Received(1).Validate(directions);
+        }
+    }
+}
---
+++ Tests/DirectionsReductor.Application.Services.Tests/Validation/ValidationServiceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace DirectionsReductor.Application.Services.Tests.Validation
+{
+    using AutoFixture;
+    using DirectionsReductor.Application.Services.Validation;
+    using System;
+    using System.Linq;
+    using Xunit;
+
+    public class ValidationServiceTests
+    {
+        private readonly Fixture fixture;
+        private readonly IValidationService validationService;
+
+        public ValidationServiceTests()
+        {
+            fixture = new Fixture();
+            validationService = new ValidationService();
+        }
+
+        [Fact]
+        public void Validate_InvalidInputDirections_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            var directions = fixture.CreateMany<string>();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => validationService.Validate(directions.ToList()));
+        }
+    }
+}
